ba55be045c7b534aebb30c0ea35cee567bd5aa37
Piotr Gaszewski <piotr.gaszewski@onedynamics.pl>
2024-04-02 15:22:54 +0200

    Initial commit for oncoming blog-post related code

---
+++ create-update-multiple-demo/src/Odx.Demo.MultipleEvents/Plugins/MultipleNextContactDatesUpdateHandler.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Odx.Demo.MultipleEvents.Plugins
+{
+    internal class MultipleNextContactDatesUpdateHandler : PluginBase
+    {
+        public MultipleNextContactDatesUpdateHandler(Type pluginClassName) : base(pluginClassName)
+        {
+        }
+    }
+}
